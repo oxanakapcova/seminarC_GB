a1c144747e2d9e4a4822997c049418dc077a8ce3
oxanakapcova <sakhaoks@hotmail.com>
2022-12-02 11:43:18 +0300

    добавлен вывод на консольдвумурного массива пользователем

---
+++ array2Input/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+Console.WriteLine("Hello, Array Input!");
+int [,] myarray;
+int line, column;
+System.Console.WriteLine("Enter amount of lines: ");
+line = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter amount of columns: ");
+column = Convert.ToInt32(Console.ReadLine());
+myarray = new int [line,column];
+
+for (int i = 0; i < myarray.GetLength(0); i++)
+{
+    for (int j = 0; j < myarray.GetLength(1); j++)
+    {
+        System.Console.Write("Y"+i+"; X"+j+"\t");
+        myarray[i,j] = Convert.ToInt32(Console.ReadLine());
+    }
+    System.Console.WriteLine();
+}//заполнили массив с консоли
+
+for (int i = 0; i < myarray.GetLength(0); i++)
+{
+    for (int j = 0; j < myarray.GetLength(1) ; j++)
+    {
+       System.Console.Write(myarray[i,j]+"\t"); 
+    }
+    System.Console.WriteLine();
+}//вывели на консоль массив
---
+++ arrayRandom2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("Hello, Random Array!");
+int [,] myarray = new int[3,4];
+Random myrandom = new Random();//переменная 
+for (int h = 0; h < myarray.GetLength(0) ; h++)
+{
+    for (int w = 0; w < myarray.GetLength(1); w++)
+    {
+        myarray[h,w] = myrandom.Next(100);//заполнили переменной случайными
+    }//числами до 100
+}
+for (int h = 0; h < myarray.GetLength(0) ; h++)
+{//в этой части вывод на консоль
+    for (int w = 0; w < myarray.GetLength(1) ; w++)
+    {
+        System.Console.Write(myarray[h,w]+"\t");
+    }
+    System.Console.WriteLine();//отделяет строки
+}
