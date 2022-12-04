519f25ce8f9377a7eddd62de15e4470abfa3e199
oxanakapcova <sakhaoks@hotmail.com>
2022-12-04 13:19:40 +0300

    добавлено описание синтаксиса метода return

---
+++ MethodSintax/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Hello, Methods!");
+//modificator|type|name|(arguments)
+static        int  sum  (int value1, int value2)
+{//method body
+    int result = value1 + value2;//return v1+v2; |same but shorter
+    return result;//ключевое слово которое впоследствии вызывается
+}//created method sum
+
+int a, b, c;
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+//variable|= name of method|(arguments)|;
+c          = sum            (a, b);
+System.Console.WriteLine(c);//variable
